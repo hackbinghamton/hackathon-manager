59ad64ba9c60c226c8b8c0b6803eeab275d1a90b
Koopa <codingkoopa@gmail.com>
2023-11-23 03:59:34 -0500

    home/page: nginx

--- home/src/routes/+page.svelte
+++ home/src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+	If you see this page, the nginx web server is successfully installed and working. Further
+	configuration is required.
+</p>
