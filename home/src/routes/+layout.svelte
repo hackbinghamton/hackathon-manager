d70f188da3d65eb71d4d02c9d77536e85e7584fb
Koopa <codingkoopa@gmail.com>
2023-11-24 01:20:22 -0500

    home/style: more padding on mobile

--- home/src/routes/+layout.svelte
+++ home/src/routes/+layout.svelte
@@ -25,7 +25,7 @@
 <!-- We need to make this transparent since a child has a rounded border. -->
 <Navbar
 	fluid={true}
-	class="mb-2.5 border-b border-gray-300 dark:border-gray-600 sm:rounded-lg sm:border"
+	class="mb-2.5 border-b border-gray-300 px-4 dark:border-gray-600 sm:rounded-lg sm:border"
 >
 	<NavBrand href="/">
 		<img src={logo_icon} class="-my-2 mr-1 h-12 sm:-my-1 sm:mr-3 sm:h-14" alt="HackBU Logo" />
