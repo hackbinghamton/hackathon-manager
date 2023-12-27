e4934d686d87adc2ff23c21d182c566e33972457
Koopa <codingkoopa@gmail.com>
2023-12-27 00:13:41 -0500

    home/src: standardize message type

--- home/src/app.d.ts
+++ home/src/app.d.ts
@@ -4,8 +4,17 @@ declare global {
 	namespace App {
 		// interface Error {}
 		// interface Locals {}
-		// interface PageData {}
 		// interface Platform {}
+		namespace Superforms {
+			type Message = {
+				type: 'error' | 'success';
+				text: string;
+			};
+		}
+		interface PageData {
+			// See: https://github.com/ciscoheat/sveltekit-flash-message.
+			flash?: Superforms.Message;
+		}
 	}
 }
 
