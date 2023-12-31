86e54ff49e8266311475acfa586cd4c045bc67ee
Koopa <codingkoopa@gmail.com>
2023-12-31 04:56:55 -0500

    home/layout: add user auth (oops)

--- home/src/routes/+layout.server.ts
+++ home/src/routes/+layout.server.ts
@@ -1,7 +1,10 @@
 import { loadFlash } from 'sveltekit-flash-message/server';
 
-export const load = loadFlash(async (event) => {
-	// See: https://authjs.dev/getting-started/providers/oauth-tutorial?frameworks=sveltekit#exposing-the-session-via-page-store
-	const data = { session: await event.locals.getSession() };
-	return data;
+export const load = loadFlash(async ({ locals }) => {
+	const session = await locals.authRequest.validate();
+	if (session) {
+		return {
+			user: session.user
+		};
+	}
 });
