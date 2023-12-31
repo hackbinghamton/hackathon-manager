aa9492fa4183723c36af4dd80f63f022740faf78
Koopa <codingkoopa@gmail.com>
2023-12-31 04:44:20 -0500

    home/page: add google sign-in button

--- home/src/routes/+page.svelte
+++ home/src/routes/+page.svelte
@@ -1,4 +1,10 @@
+<script lang="ts">
+	import SignInButtonGoogle from '$lib/components/SignInButtonGoogle.svelte';
+</script>
+
 <p>
 	If you see this page, the nginx web server is successfully installed and working. Further
 	configuration is required.
 </p>
+
+<SignInButtonGoogle />
